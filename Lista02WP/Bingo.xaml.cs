64345cc6b635e862b480c22fcec21ab4593abc43
Daniel Souza Affonso Ferreira <daniel.souza.af@gmail.com>
2016-07-25 23:44:53 -0300

    Add project files.

---
+++ Lista02WP/Bingo.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace Lista02WP
+{
+    public partial class Bingo : PhoneApplicationPage
+    {
+        public Bingo()
+        {
+            InitializeComponent();
+        }
+
+        private void sldQtd_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+           
+        }
+
+        private void sldQtd_LostMouseCapture(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            var passo = 10;
+            var meiopasso = passo / 10; //0
+
+            var distanciafim = sldQtd.Value % passo; //76 % 10 == 6
+            if (distanciafim > meiopasso) // 0.6 > 1
+                distanciafim = distanciafim - passo;
+
+            sldQtd.Value -= distanciafim;
+            txtQtd.Text = sldQtd.Value.ToString();
+        }
+    }
+}
---
+++ Lista02WP/Combustivel.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace Lista02WP
+{
+    public partial class Combustivel : PhoneApplicationPage
+    {
+        public Combustivel()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, RoutedEventArgs e)
+        {
+            double qtdLitros = double.Parse(txtVP.Text) / double.Parse(txtVL.Text);
+            txtKML.Text = (double.Parse(txtKm.Text) / qtdLitros).ToString();
+            txtRSKM.Text = (double.Parse(txtVP.Text) / double.Parse(txtKm.Text)).ToString();
+        }
+    }
+}
